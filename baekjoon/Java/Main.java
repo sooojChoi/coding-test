feed1c20127004f02af03054a228719fc969e2dc
sooojChoi <cmleon88@naver.com>
2023-12-13 15:14:14 +0900

    test

---
+++ baekjoon/Java/Main.java
@@ -0,0 +1,11 @@
+package baekjoon.Java;
+
+public class Main {
+    public static void main(String[] args) {
+        if(args.length > 0){
+            for(String str : args){
+                System.out.println(str);
+            }
+        }
+    }
+}

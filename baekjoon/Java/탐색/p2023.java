47d74d6a9906c5e90c81f360f23cba36b0c1d50d
sooojChoi <cmleon88@naver.com>
2024-01-21 23:54:33 +0900

    DFS

--- baekjoon/Java/탐색/p2023.java
+++ baekjoon/Java/탐색/p2023.java
@@ -1,28 +1,55 @@
 package baekjoon.Java.탐색;
 
-import java.util.*;
 import java.io.*;
 
 // DFS
 public class p2023 {
+    static int N;
     public static void main(String[] args){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        try{
+            N = Integer.parseInt(br.readLine());
+
+            DFS(2,1);
+            DFS(3,1);
+            DFS(5,1);
+            DFS(7,1);
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
 
 
 
-    boolean primeTest(int n){
+    static boolean primeTest(long n){
         if(n==1)
             return false;
         if(n==2)
             return true;
 
-        int mid = n/2;
-        for(int i=2;i<mid;i++){
+        long mid = n/2;
+        for(long i=2;i<mid;i++){
             if(n%i==0){
                 return false;
             }
         }
         return true;
+    }
+
+    static void DFS(long v, int jarisu){
+        if(jarisu == N){
+            System.out.println(v);
+            return;
+        }
+
+        for(int i=1;i<=9;i++){
+            // 소수인지 검사하고 소수이면 그 아래까지 탐색.
+            long nv = v*10+i;
+            if(primeTest(nv)){
+                DFS(nv, jarisu+1);
+            }
+        }
     }
 }

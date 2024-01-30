496da4d397834fe4c5f18b760fc4a532aefd3f0a
sooojChoi <cmleon88@naver.com>
2024-01-30 15:58:23 +0900

    dijkstra

--- baekjoon/Java/그래프/newP1516.java
+++ baekjoon/Java/그래프/newP1516.java
@@ -6,65 +6,70 @@ import java.io.*;
 public class newP1516 {
     static ArrayList<Integer>[] arr;
     static int[] time;
-    public static void main(String[] args){
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N;
-        int[] in_nodes_num;  //진입차수 저장하는 배열
-
-        try{
-            N = Integer.parseInt(br.readLine());
-            arr = new ArrayList[N+1];
-            time = new int[N+1];
-            
-            in_nodes_num = new int[N+1];
-
-            for(int i=1;i<=N;i++){
-                arr[i] = new ArrayList<Integer>();
-            }
-            StringTokenizer st;
-            for(int i=1;i<=N;i++){
-                st = new StringTokenizer(br.readLine());
-                time[i]=Integer.parseInt(st.nextToken());
-
-                while(true){
-                    int n = Integer.parseInt(st.nextToken());
-                    if(n==-1){
-                        break;
+    public class Main {
+        static ArrayList<Integer>[] arr;
+        static int[] time;
+        public static void main(String[] args){
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N;
+            int[] in_nodes_num;  //진입차수 저장하는 배열
+    
+            try{
+                N = Integer.parseInt(br.readLine());
+                arr = new ArrayList[N+1];
+                time = new int[N+1];
+                
+                in_nodes_num = new int[N+1];
+    
+                for(int i=1;i<=N;i++){
+                    arr[i] = new ArrayList<Integer>();
+                }
+                StringTokenizer st;
+                for(int i=1;i<=N;i++){
+                    st = new StringTokenizer(br.readLine());
+                    time[i]=Integer.parseInt(st.nextToken());
+    
+                    while(true){
+                        int n = Integer.parseInt(st.nextToken());
+                        if(n==-1){
+                            break;
+                        }
+                        // 그래프 초기화
+                        arr[n].add(i);
+                        // 진입차수 초기화
+                        in_nodes_num[i]++;
                     }
-                    // 그래프 초기화
-                    arr[n].add(i);
-                    // 진입차수 초기화
-                    in_nodes_num[i]++;
                 }
-            }
-
-            Queue<Integer> q = new LinkedList<Integer>();
-            for(int i=1;i<=N;i++){
-                if(in_nodes_num[i]==0){
-                    q.add(i);
-                }
-            }
-
-            int node;
-            int[] result = new int[N+1];
-            while(!q.isEmpty()){
-                node = q.poll();
-
-                for(int i:arr[node]){
-                    result[i] = Math.max(result[i], result[node]+time[node]);
-               
-                    in_nodes_num[i]--;
+    
+                Queue<Integer> q = new LinkedList<Integer>();
+                for(int i=1;i<=N;i++){
                     if(in_nodes_num[i]==0){
                         q.add(i);
                     }
                 }
+    
+                int node;
+                int[] result = new int[N+1];
+                while(!q.isEmpty()){
+                    node = q.poll();
+    
+                    for(int i:arr[node]){
+                        result[i] = Math.max(result[i], result[node]+time[node]);
+                   
+                        in_nodes_num[i]--;
+                        if(in_nodes_num[i]==0){
+                            q.add(i);
+                        }
+                    }
+                }
+    
+                for(int i=1;i<=N;i++){
+                    System.out.println(result[i]+time[i]);
+                }
+            }catch(Exception e){
+                System.out.println(e);
             }
-
-            for(int i=1;i<=N;i++){
-                System.out.println(result[i]+time[i]);
-            }
-        }catch(Exception e){
-            System.out.println(e);
         }
     }
+    
 }
---
+++ baekjoon/Java/그래프/p1753.java
@@ -0,0 +1,93 @@
+package baekjoon.Java.그래프;
+
+import java.util.*;
+import java.io.*;
+
+// 다익스트라
+public class p1753 {
+    public static void main(String[] args){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int V, E, K;
+        ArrayList<Edge>[] arr;
+        int[] distance;
+        boolean[] visited;
+
+        try{
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            V = Integer.parseInt(st.nextToken());
+            E = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(br.readLine());
+            arr = new ArrayList[V+1];
+            distance = new int[V+1];
+            visited = new boolean[V+1];
+
+            for(int i=1;i<=V;i++){
+                arr[i] = new ArrayList<>();
+                distance[i] = Integer.MAX_VALUE;
+            }
+
+            for(int i=0;i<E;i++){
+                st = new StringTokenizer(br.readLine());
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                int w = Integer.parseInt(st.nextToken());
+
+                arr[s].add(new Edge(e,w));
+            }
+
+            PriorityQueue<Edge> q = new PriorityQueue<Edge>();
+            q.add(new Edge(K,0));
+            distance[K]=0;
+
+            // q가 비어있지 않을 때까지
+            while(!q.isEmpty()){
+                Edge edge = q.poll();  // 가장 작은 distance를 가진 노드 선택.
+                if(visited[edge.node]){
+                    // 이미 방문한 노드라면 계산하지 않음.
+                    continue;
+                }
+                visited[edge.node] = true;
+
+                for(Edge e:arr[edge.node]){ 
+                    if(distance[e.node] > distance[edge.node]+e.weight){
+                        distance[e.node] = distance[edge.node]+e.weight;
+                        q.add(new Edge(e.node, distance[e.node]));
+                    }
+                }
+            }
+
+            for(int i=1;i<=V;i++){
+                if(visited[i]){
+                    System.out.println(distance[i]);
+                }else{
+                    System.out.println("INF");
+                }
+            }
+
+
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    
+}
+
+class Edge implements Comparable<Edge>{
+    int node;
+    int weight;
+
+    Edge(int node, int weight){
+        this.node = node;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        if(this.weight - o.weight <0){
+            return -1;
+        }else{
+            return 1;
+        }
+    }
+}

4ed559bb2154aa84a33bbb63c8397a6c08d5b4f6
sooojChoi <cmleon88@naver.com>
2024-04-24 16:44:19 +0900

    sw1244

--- SWExpertAcademy/sw1244.java
+++ SWExpertAcademy/sw1244.java
@@ -4,7 +4,8 @@ import java.util.*;
 
 // 완전 탐색으로 풀어도 최대 6의 10승번 이기 때문에 문제 조건에 부합함.
 public class sw1244 {
-    static int max=0;
+    static int answer=0;
+    static int max;
     static Set set = new HashSet<Integer>();
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
@@ -21,10 +22,13 @@ public class sw1244 {
             for(int i=0;i<list.length;i++){
                 list[i] = Integer.parseInt(str.substring(i, i+1));
             }
-    
+            int[] clone = list.clone();
+            Arrays.sort(clone);
+            max = getIntFromArray(clone);
+
             changeList(list, change);
     
-            System.out.println("#"+test_case+" "+max);
+            System.out.println("#"+test_case+" "+answer);
 		}
         
     }
@@ -36,29 +40,37 @@ public class sw1244 {
                 list[i]=list[j];
                 list[j] = temp;
 
-                int x=0;
-                for(int k=list.length-1;k>=0;k--){
-                    x += (Math.pow(10, k)*list[Math.abs(list.length-1-k)]);
-                }
-                if(set.contains(x)){
-                    continue;
-                }else{
-                    set.add(x);
-                }
+                // int x=getIntFromArray(list);
+                // if(x==max){
+                //     answer = max;
+                //     return;
+                // }
+                // x가 최적해인지 검사하고, 최적해라면 max에 저장한 후 return 한다.
+                
 
                 // 주어진 횟수만큼 다 수행했다면,
                 if(c==0){
+                    int x;
                     x=0;
                     for(int k=list.length-1;k>=0;k--){
                         x += (Math.pow(10, k)*list[Math.abs(list.length-1-k)]);
                     }
-                    if(x>max){
-                        max = x;
+                    if(x>answer){
+                        answer = x;
                     }
                 }else{
                     changeList(list.clone(), c);
                 }
             }   
         }
+    }
+
+    public static int getIntFromArray(int[] array){
+        int x=0;
+        for(int k=array.length-1;k>=0;k--){
+            x += (Math.pow(10, k)*array[Math.abs(array.length-1-k)]);
+        }
+
+        return x;
     }
 }

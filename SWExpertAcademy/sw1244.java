fad7d2855dd22908e6732c23a3a6934f67472cb4
sooojChoi <cmleon88@naver.com>
2024-04-27 19:28:28 +0900

    sw1244 solved

--- SWExpertAcademy/sw1244.java
+++ SWExpertAcademy/sw1244.java
@@ -2,11 +2,36 @@ package SWExpertAcademy;
 
 import java.util.*;
 
-// 완전 탐색으로 풀어도 최대 6의 10승번 이기 때문에 문제 조건에 부합함.
+// input
+// 10
+// 123 1
+// 2737 1
+// 757148 1
+// 78466 2
+// 32888 2
+// 777770 5
+// 436659 2
+// 431159 7
+// 112233 3
+// 456789 10
+
+//output
+// #1 321
+// #2 7732
+// #3 857147
+// #4 87664
+// #5 88832
+// #6 777770
+// #7 966354
+// #8 954311
+// #9 332211
+// #10 987645
+
 public class sw1244 {
     static int answer=0;
     static int max;
-    static Set set = new HashSet<Integer>();
+    static boolean findAnswer = false;
+
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         int T;
@@ -22,46 +47,94 @@ public class sw1244 {
             for(int i=0;i<list.length;i++){
                 list[i] = Integer.parseInt(str.substring(i, i+1));
             }
-            int[] clone = list.clone();
-            Arrays.sort(clone);
-            max = getIntFromArray(clone);
+        
+            Integer[] clone = Arrays.stream(list).boxed().toArray(Integer[]::new); 
+            Arrays.sort(clone, Collections.reverseOrder());
+            int x=0;
+            for(int k=clone.length-1;k>=0;k--){
+                x += (Math.pow(10, k)*clone[Math.abs(clone.length-1-k)]);
+            }
+            max = x;
 
             changeList(list, change);
     
             System.out.println("#"+test_case+" "+answer);
+            answer = 0;
+            findAnswer = false;
 		}
+
+        sc.close();
         
     }
     public static void changeList(int[] list, int c){
+        
+        if(c==0){
+            return;
+        }
         c--;
         for(int i=0;i<list.length-1;i++){
             for(int j=i+1;j<list.length;j++){
-                int temp = list[i];
-                list[i]=list[j];
-                list[j] = temp;
-
-                // int x=getIntFromArray(list);
-                // if(x==max){
-                //     answer = max;
-                //     return;
-                // }
-                // x가 최적해인지 검사하고, 최적해라면 max에 저장한 후 return 한다.
-                
+                int[] cloneList = list.clone();
+                int temp = cloneList[i];
+                cloneList[i]=cloneList[j];
+                cloneList[j] = temp;
 
                 // 주어진 횟수만큼 다 수행했다면,
                 if(c==0){
                     int x;
-                    x=0;
-                    for(int k=list.length-1;k>=0;k--){
-                        x += (Math.pow(10, k)*list[Math.abs(list.length-1-k)]);
-                    }
+                    x = getIntFromArray(cloneList);
                     if(x>answer){
                         answer = x;
+                        if(answer == max){
+                            findAnswer = true;
+                        }
                     }
                 }else{
-                    changeList(list.clone(), c);
+                    // 교환한 것이 최적해이면, 더 구하지 않는다.
+                    int x = getIntFromArray(cloneList);
+                    if(x==max){
+                        // 남은 횟수가 짝수일 경우
+                        if(c%2==0){
+                            answer = max;
+                        }else{
+                            boolean duplicate = false;
+                            for(int k=0;k<cloneList.length;k++){
+                                for(int z=0;z<cloneList.length;z++){
+                                    if(k!=z){
+                                        if(k==z){
+                                            duplicate = true;
+                                            break;
+                                        }
+                                    }
+                                }
+                                if(duplicate){
+                                    break;
+                                }
+                            }
+
+                            // 남은 횟수가 홀수이고, 중복되는 값이 있는 경우
+                            if(duplicate){
+                                answer = max;
+                            }else{
+                                // 마지막 두 요소의 값을 교환한 값이 답이다. 
+                                int t = cloneList[cloneList.length-1];
+                                cloneList[cloneList.length-1] = cloneList[cloneList.length-2];
+                                cloneList[cloneList.length-2] = t;
+
+                                answer = getIntFromArray(cloneList);
+                            }
+                        }
+                        findAnswer = true;
+                        break;
+                    }
+
+                    changeList(cloneList.clone(), c);
                 }
             }   
+            // 최적해를 구했으면, 더 이상 탐색하지 않고 중단한다. 
+            if(findAnswer){
+                break;
+            }
         }
     }
 
@@ -74,3 +147,64 @@ public class sw1244 {
         return x;
     }
 }
+
+
+// Java로 다르게 풀이된 코드
+// 위처럼 여러 조건 생각할 필요 없이, 방문 체크하며 완전탐색하면 된다.
+// 몇 번째의 특정 숫자를 이미 검사한 적 있다면 또 검사하지 않도록 한다.
+
+// import java.util.Scanner;
+// import java.io.FileInputStream;
+// import java.util.*;
+ 
+// /*
+//    사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+//    이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+//  */
+// class Solution
+// {
+//     static int exchangeCount ;
+//     static int result ;
+//     static boolean[][] visited;
+//     public static void main(String args[]) throws Exception
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         int T;
+//         T=sc.nextInt();
+//         for(int test_case = 1; test_case <= T; test_case++)
+//         {
+//             String number = sc.next();
+//             exchangeCount = sc.nextInt();
+//             result = Integer.MIN_VALUE;
+//             visited = new boolean[exchangeCount][(int) Math.pow(10, number.length())];
+//             getMaxNumber(number, 0);
+//             System.out.printf("#%d %d\n",test_case, result);
+//         }
+//     }
+//     private static void getMaxNumber(String number, int count) {
+//         if (count == exchangeCount) {
+//             result = Math.max(Integer.parseInt(number), result);
+//             return;
+//         }
+//         for (int i = 0; i < number.length(); i++) {
+//             char temp1 = number.charAt(i);
+//             for (int j = i + 1; j < number.length(); j++) {
+//                 char temp2 = number.charAt(j);
+//                 StringBuilder sb = new StringBuilder();
+//                 for (int k = 0; k < number.length(); k++) {
+//                     if (k == i) {
+//                         sb.append(temp2);
+//                     } else if (k == j) {
+//                         sb.append(temp1);
+//                     } else {
+//                         sb.append(number.charAt(k));
+//                     }
+//                 }
+//                 if (!visited[count][Integer.parseInt(sb.toString())]) {
+//                     visited[count][Integer.parseInt(sb.toString())] = true;
+//                     getMaxNumber(sb.toString(), count + 1);
+//                 }
+//             }
+//         }
+//     }
+// }
